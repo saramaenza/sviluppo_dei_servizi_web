86c22ea5066dc668ed3a4281ba36597dee360032
saramaenza <s.maenza1@studenti.unipi.it>
2022-02-22 18:50:32 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-znndg3
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-znndg3)
---
+++ index.js
@@ -0,0 +1,6 @@
+// Import stylesheets
+import './style.css';
+
+document.getElementById("demo").innerHTML = "Primo<br>";
+document.getElementById("demo").innerHTML += "Secondo<br>";
+document.getElementById("demo").innerHTML += "<b>Terzo</b>";
